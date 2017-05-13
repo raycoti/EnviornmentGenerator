b9af077af38a7a4c98531207b0d7d4cffb7a12f5
ray coti <Raymond.coti@gmail.com>
2017-05-12 19:06:59 -0500

    adds enviornment variable

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
